8c536615afa43dbb5c83fb465a3213f0f65c63ad
Jürg Lehni <juerg@scratchdisk.com>
2005-03-05 22:13:05 +0000

    added Windows compatibility

--- src/cpp/plugin/resourceIds.h
+++ src/cpp/plugin/resourceIds.h
@@ -26,46 +26,46 @@
  *
  * $RCSfile: resourceIds.h,v $
  * $Author: lehni $
- * $Revision: 1.1 $
- * $Date: 2005/02/23 22:00:59 $
+ * $Revision: 1.2 $
+ * $Date: 2005/03/05 22:13:05 $
  */
 
-#define kFolderPictureID		128
-#define kPlayPictureID			129
-#define kScriptPictureID		130
-#define kSavePictureID			131
-#define kRefreshPictureID		132
-#define kConsolePictureID		133
-#define kTool1IconID			134
-#define kTool2IconID			135
-#define kTool1ScriptPictureID	136
-#define kTool2ScriptPictureID	137
-#define kStopPictureID			138
+#define kFolderPictureID				128
+#define kPlayPictureID					129
+#define kScriptPictureID				130
+#define kSavePictureID					131
+#define kRefreshPictureID				132
+#define kConsolePictureID				133
+#define kTool1IconID					134
+#define kTool2IconID					135
+#define kTool1ScriptPictureID			136
+#define kTool2ScriptPictureID			137
+#define kStopPictureID					138
 
-#define kTool1CursorID			128
-#define kTool2CursorID			129
+#define kTool1CursorID					128
+#define kTool2CursorID					129
 
-#define kMainPulldownMenuID		700
-#define kEditorPulldownMenuID	701
+#define kMainPulldownMenuID				700
+#define kEditorPulldownMenuID			701
 
-#define kMainScriptListItem		1
-#define kMainPlayItem			2
-#define kMainStopItem			3
-#define kMainRefreshItem		4
-#define kMainEditorItem			5
-#define kMainConsoleItem		6
-#define kMainTool1Item			7
-#define kMainTool2Item			8
-#define kMainBaseDirItem		9
-#define	kAboutItem				10
-#define kEditorFileNameItem		1
-#define kEditorTextItem			2
-#define kEditorPlayItem			3
-#define kEditorStopItem			4
-#define kEditorSaveItem			5
-#define kEditorNewItem			6
-#define kEditorLineItem			7
-#define kConsoleTextItem		1
+#define kMainScriptListItem				1
+#define kMainPlayItem					2
+#define kMainStopItem					3
+#define kMainRefreshItem				4
+#define kMainEditorItem					5
+#define kMainConsoleItem				6
+#define kMainTool1Item					7
+#define kMainTool2Item					8
+#define kMainBaseDirItem				9
+#define	kAboutItem						10
+#define kEditorFileNameItem				1
+#define kEditorTextItem					2
+#define kEditorPlayItem					3
+#define kEditorStopItem					4
+#define kEditorSaveItem					5
+#define kEditorNewItem					6
+#define kEditorLineItem					7
+#define kConsoleTextItem				1
 
 #define kMainDialogID                   16000
 #define kEditorDialogID                 16001
@@ -77,10 +77,10 @@
 #define kAboutDialogID					16050
 #define kErrorDialogID					16051
 
-#define kErrorStrings		16050
+#define kErrorStrings					16050
 
-#define kDlgOKButton		1
-#define kDlgCancelButton	2
+#define kDlgOKButton					1
+#define kDlgCancelButton				2
 
-#define kErrorMessageText	2
-#define kErrorCodeText      3
+#define kErrorMessageText				2
+#define kErrorCodeText					3
