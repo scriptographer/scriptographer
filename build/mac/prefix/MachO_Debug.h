385d833d63591ecdd38775749d6322c5bbf8f971
Jürg Lehni <juerg@scratchdisk.com>
2006-10-18 14:18:20 +0000

    Updated copyright information

--- build/mac/prefix/MachO_Debug.h
+++ build/mac/prefix/MachO_Debug.h
@@ -3,7 +3,7 @@
  *
  * This file is part of Scriptographer, a Plugin for Adobe Illustrator.
  *
- * Copyright (c) 2002-2005 Juerg Lehni, http://www.scratchdisk.com.
+ * Copyright (c) 2002-2006 Juerg Lehni, http://www.scratchdisk.com.
  * All rights reserved.
  *
  * Please visit http://scriptographer.com/ for updates and contact.
@@ -26,8 +26,8 @@
  *
  * $RCSfile: MachO_Debug.h,v $
  * $Author: lehni $
- * $Revision: 1.1 $
- * $Date: 2006/06/07 16:44:15 $
+ * $Revision: 1.2 $
+ * $Date: 2006/10/18 14:18:41 $
  */
 
 #ifndef __machoDebug_h__	// Has this not been defined yet?
