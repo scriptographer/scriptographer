e9bf886416d2f23e50fdfa1ac23733019207b6ac
Jürg Lehni <juerg@scratchdisk.com>
2006-12-11 18:56:36 +0000

    Checked in free syntax coloring component from jedit, with some bug from Processing.org

---
+++ src/java/com/scriptographer/syntax/XMLTokenMarker.java
@@ -0,0 +1,22 @@
+/*
+ * XMLTokenMarker.java - XML token marker
+ * Copyright (C) 1999 Slava Pestov
+ *
+ * You may use and modify this package for any purpose. Redistribution is
+ * permitted, in both source and binary form, provided that this notice
+ * remains intact in all source distributions of this package.
+ */
+
+package com.scriptographer.syntax;
+
+/**
+ * XML token marker.
+ * 
+ * @author Slava Pestov
+ * @version $Id: XMLTokenMarker.java,v 1.1 2006/12/11 18:56:37 lehni Exp $
+ */
+public class XMLTokenMarker extends HTMLTokenMarker {
+	public XMLTokenMarker() {
+		super(false);
+	}
+}
