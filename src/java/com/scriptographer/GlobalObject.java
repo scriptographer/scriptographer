c8056c44eb0ced4e1aefe2e2e4d83b77eab5aa54
Jürg Lehni <juerg@scratchdisk.com>
2006-12-11 18:52:15 +0000

    Clean-up class exposure code, add java.util.prefs support, various changes

--- src/java/com/scriptographer/GlobalObject.java
+++ src/java/com/scriptographer/GlobalObject.java
@@ -28,22 +28,144 @@
  * 
  * $RCSfile: GlobalObject.java,v $
  * $Author: lehni $
- * $Revision: 1.19 $
- * $Date: 2006/10/18 14:07:59 $
+ * $Revision: 1.20 $
+ * $Date: 2006/12/11 18:52:15 $
  */
 
 package com.scriptographer;
 
 import org.mozilla.javascript.*;
+import org.mozilla.javascript.tools.debugger.ScopeProvider;
 
 import com.scriptographer.js.ExtendedJavaClass;
 import com.scriptographer.ai.*;
 import com.scriptographer.adm.*;
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.prefs.Preferences;
 
-public class GlobalObject extends ImporterTopLevel {
+public class GlobalObject extends ImporterTopLevel implements ScopeProvider {
+
+	final static Class classes[] = {
+		// ADM, alphabetically
+		Button.class,
+		ChasingArrows.class,
+		Dial.class,
+		CheckBox.class,
+		Dialog.class,
+		DialogGroupInfo.class,
+		Drawer.class,
+		FloatingDialog.class,
+		FontInfo.class,
+		Frame.class,
+		HierarchyList.class,
+		HierarchyListEntry.class,
+		Image.class,
+		ImageButton.class,
+		ImageCheckBox.class,
+		ImageRadioButton.class,
+		ImageStatic.class,
+		Item.class,
+		ItemContainer.class,
+		ItemGroup.class,
+		Key.class,
+		List.class,
+		ListEntry.class,
+		ListItem.class,
+		MenuGroup.class,
+		MenuItem.class,
+		ModalDialog.class,
+		PopupDialog.class,
+		PopupList.class,
+		PopupMenu.class,
+		ProgressBar.class,
+		RadioButton.class,
+		ScrollBar.class,
+		Slider.class,
+		Spacer.class,
+		SpinEdit.class,
+		Static.class,
+		TableLayout.class,
+		TextEdit.class,
+		TextItem.class,
+		TextValueItem.class,
+		ToggleItem.class,
+		Tracker.class,
+
+		// AWT Layout classes
+		java.awt.BorderLayout.class,
+		java.awt.Dimension.class,
+		java.awt.FlowLayout.class,
+		java.awt.Insets.class,
+
+		// AI, alphabetically
+		Annotator.class,
+		AreaText.class,
+		Art.class,
+		ArtSet.class,
+		CharacterStyle.class,
+		CMYKColor.class,
+		Color.class,
+		CompoundPath.class,
+		Curve.class,
+		CurveList.class,
+		Document.class,
+		DocumentList.class,
+		Event.class,
+		FileFormat.class,
+		FillStyle.class,
+		FontFamily.class,
+		FontList.class,
+		FontWeight.class,
+		Gradient.class,
+		GradientColor.class,
+		GradientList.class,
+		GradientStop.class,
+		GradientStopList.class,
+		GrayColor.class,
+		Group.class,
+		HitTest.class,
+		Layer.class,
+		LayerList.class,
+		LiveEffect.class,
+		Matrix.class,
+		ParagraphStyle.class,
+		Path.class,
+		Pathfinder.class,
+		PathStyle.class,
+		PathText.class,
+		Pattern.class,
+		PatternColor.class,
+		PatternList.class,
+		Point.class,
+		PointText.class,
+		Raster.class,
+		Rectangle.class,
+		RGBColor.class,
+		Segment.class,
+		SegmentList.class,
+		StrokeStyle.class,
+		Swatch.class,
+		SwatchList.class,
+		Symbol.class,
+		SymbolItem.class,
+		SymbolList.class,
+		TabletValue.class,
+		TextFrame.class,
+		TextRange.class,
+		TextStory.class,
+		Timer.class,
+		Tool.class,
+		Tracing.class,
+		View.class,
+		ViewList.class,
+
+		// Java
+		File.class
+	};
 
 	protected GlobalObject(Context context) {
 		super(context);
@@ -53,123 +175,37 @@ public class GlobalObject extends ImporterTopLevel {
 		// is created as prototype or as real object through
 		// isCreatingPrototypes()
 
-		// Global
-		new ExtendedJavaClass(this, Key.class);
-		
-		// ADM
-		new ExtendedJavaClass(this, Dialog.class);
-		new ExtendedJavaClass(this, ModalDialog.class);
-		new ExtendedJavaClass(this, FloatingDialog.class);
-		new ExtendedJavaClass(this, PopupDialog.class);
-		new ExtendedJavaClass(this, Drawer.class);
-		new ExtendedJavaClass(this, Tracker.class);
-		new ExtendedJavaClass(this, Image.class);
-		new ExtendedJavaClass(this, ListItem.class);
-		new ExtendedJavaClass(this, ListEntry.class);
-		new ExtendedJavaClass(this, HierarchyList.class);
-		new ExtendedJavaClass(this, HierarchyListEntry.class);
-		// All item classes:
-		new ExtendedJavaClass(this, Frame.class);
-		new ExtendedJavaClass(this, ItemGroup.class);
-		new ExtendedJavaClass(this, List.class);
-		new ExtendedJavaClass(this, HierarchyList.class);
-		new ExtendedJavaClass(this, Button.class);
-		new ExtendedJavaClass(this, CheckBox.class);
-		new ExtendedJavaClass(this, RadioButton.class);
-		new ExtendedJavaClass(this, Static.class);
-		new ExtendedJavaClass(this, ImageButton.class);
-		new ExtendedJavaClass(this, ImageCheckBox.class);
-		new ExtendedJavaClass(this, ImageRadioButton.class);
-		new ExtendedJavaClass(this, ImageStatic.class);
-		new ExtendedJavaClass(this, ScrollBar.class);
-		new ExtendedJavaClass(this, Slider.class);
-		new ExtendedJavaClass(this, ProgressBar.class);
-		new ExtendedJavaClass(this, TextEdit.class);
-		new ExtendedJavaClass(this, Dial.class);
-		new ExtendedJavaClass(this, ChasingArrows.class);
-		new ExtendedJavaClass(this, PopupList.class);
-		new ExtendedJavaClass(this, PopupMenu.class);
-		new ExtendedJavaClass(this, SpinEdit.class);
-
-		// Layout specific classes
-		new ExtendedJavaClass(this, ItemContainer.class);
-		new ExtendedJavaClass(this, Spacer.class);
-		new ExtendedJavaClass(this, TableLayout.class);
-		new ExtendedJavaClass(this, java.awt.FlowLayout.class);
-		new ExtendedJavaClass(this, java.awt.BorderLayout.class);
-
-		// AI
-		new ExtendedJavaClass(this, Rectangle.class);
-		new ExtendedJavaClass(this, Point.class);
-		new ExtendedJavaClass(this, Matrix.class);
-
-		new ExtendedJavaClass(this, Color.class);
-		new ExtendedJavaClass(this, GrayColor.class);
-		new ExtendedJavaClass(this, RGBColor.class);
-		new ExtendedJavaClass(this, CMYKColor.class);
-
-		new ExtendedJavaClass(this, Art.class);
-		new ExtendedJavaClass(this, Path.class);
-		new ExtendedJavaClass(this, Group.class);
-		new ExtendedJavaClass(this, Raster.class);
-		new ExtendedJavaClass(this, PlacedItem.class);
-		new ExtendedJavaClass(this, Layer.class);
-		new ExtendedJavaClass(this, CompoundPath.class);
-		new ExtendedJavaClass(this, SymbolItem.class);
-		
-		new ExtendedJavaClass(this, PathStyle.class);
-		new ExtendedJavaClass(this, FillStyle.class);
-		new ExtendedJavaClass(this, StrokeStyle.class);
-
-		new ExtendedJavaClass(this, Pattern.class);
-		new ExtendedJavaClass(this, Symbol.class);
-		new ExtendedJavaClass(this, Segment.class);
-		new ExtendedJavaClass(this, Curve.class);
-		new ExtendedJavaClass(this, HitTest.class);
-		new ExtendedJavaClass(this, TabletValue.class);
-		new ExtendedJavaClass(this, Pathfinder.class);
-		new ExtendedJavaClass(this, Document.class);
-		new ExtendedJavaClass(this, LiveEffect.class);
-		new ExtendedJavaClass(this, MenuItem.class);
-		new ExtendedJavaClass(this, MenuGroup.class);
-		new ExtendedJavaClass(this, Tool.class);
-		new ExtendedJavaClass(this, Timer.class);
-		new ExtendedJavaClass(this, Annotator.class);
-		new ExtendedJavaClass(this, HitTest.class);
-
-		new ExtendedJavaClass(this, ArtSet.class);
-		new ExtendedJavaClass(this, SegmentList.class);
-		new ExtendedJavaClass(this, CurveList.class);
-		
-		new ExtendedJavaClass(this, TextFrame.class);
-		new ExtendedJavaClass(this, PointText.class);
-		new ExtendedJavaClass(this, AreaText.class);
-		new ExtendedJavaClass(this, PathText.class);
-		new ExtendedJavaClass(this, CharacterStyle.class);
-
-		new ExtendedJavaClass(this, Tracing.class);
-
-		// Java
-		new ExtendedJavaClass(this, File.class);
+		for (int i = 0; i < classes.length; i++)
+			new ExtendedJavaClass(this, classes[i]);
 
 		// define some global functions and objects:
-		String[] names = { "print", "include", "execute", "evaluate", "commit", "getNanoTime", "getMousePoint", "updateProgress" };
-		defineFunctionProperties(names, GlobalObject.class, ScriptableObject.READONLY | ScriptableObject.DONTENUM);
+		String[] names = { "print", "include", "execute", "evaluate", "commit",
+			"getNanoTime", "getMousePoint", "updateProgress" };
+		defineFunctionProperties(names, GlobalObject.class,
+			ScriptableObject.READONLY | ScriptableObject.DONTENUM);
 
 		// properties:
-		defineProperty("documents", DocumentList.getInstance(), ScriptableObject.READONLY | ScriptableObject.DONTENUM);
-		defineProperty("fonts", FontList.getInstance(), ScriptableObject.READONLY | ScriptableObject.DONTENUM);
-		defineProperty("activeDocument", "getActiveDocument", null);
-		defineProperty("scriptDir", "getScriptDirectory", null);
+		defineProperty("documents", DocumentList.getInstance(),
+			ScriptableObject.READONLY | ScriptableObject.DONTENUM);
+		defineProperty("fonts", FontList.getInstance(),
+			ScriptableObject.READONLY | ScriptableObject.DONTENUM);
+		defineProperty(this, "activeDocument", "getActiveDocument", null);
+		defineProperty(this, "scriptDir", "getScriptDirectory", null);
 	}
-	
-	protected void defineProperty(String name, String getter, String setter) {
+
+	protected static void defineProperty(ScriptableObject obj, String name,
+			String getter, String setter) {
 		try {
-			Method getterMethod = getter != null ? GlobalObject.class.getDeclaredMethod(getter, new Class[] { ScriptableObject.class }) : null;
-			Method setterMethod = setter != null ? GlobalObject.class.getDeclaredMethod(setter, new Class[] { ScriptableObject.class }) : null;
-			defineProperty(name, null, getterMethod, setterMethod, ScriptableObject.DONTENUM);
+			Method getterMethod = getter != null ? GlobalObject.class
+				.getDeclaredMethod(getter,
+					new Class[] { ScriptableObject.class }) : null;
+			Method setterMethod = setter != null ? GlobalObject.class
+				.getDeclaredMethod(setter, new Class[] {
+					ScriptableObject.class, Object.class }) : null;
+			obj.defineProperty(name, null, getterMethod, setterMethod,
+				ScriptableObject.DONTENUM);
 		} catch (Exception e) {
-			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
 	}
 
@@ -180,11 +216,13 @@ public class GlobalObject extends ImporterTopLevel {
 	protected Scriptable createScope(File scriptFile) {
 		ScriptableObject scope = new org.mozilla.javascript.NativeObject();
 		scope.setPrototype(this);
-		scope.setParentScope(null);
-		scope.defineProperty("scriptFile", scriptFile, ScriptableObject.READONLY | ScriptableObject.DONTENUM);
+		scope.setParentScope(this);
+		scope.defineProperty("scriptFile", scriptFile,
+			ScriptableObject.READONLY | ScriptableObject.DONTENUM);
+		defineProperty(scope, "preferences", "getPreferences", null);
 		return scope;
 	}
-	
+
 	/**
 	 * Determines the directory of a script by reading it's scriptFile property
 	 * in the main scope. If script file is empty (e.g. for console),
@@ -204,7 +242,7 @@ public class GlobalObject extends ImporterTopLevel {
 
 	/*
 	 * JavaScript functions
-	 *
+	 * 
 	 */
 
 	static Object getActiveDocument(ScriptableObject obj) {
@@ -214,18 +252,42 @@ public class GlobalObject extends ImporterTopLevel {
 	static Object getScriptDirectory(ScriptableObject obj) {
 		return ScriptographerEngine.getScriptDirectory();
 	}
-	
+
+	static Object getPreferences(ScriptableObject obj) throws IOException {
+		// determine preferences for the current executing script
+		// by walking up the file path to the script directory and using each
+		// folder
+		// as a preference node.
+		File file = (File) obj.get("scriptFile", obj);
+		Preferences prefs = ScriptographerEngine.getPreferences(false).node(
+			"scripts");
+		ArrayList parts = new ArrayList();
+		File root = ScriptographerEngine.getScriptDirectory();
+		// collect the directory parts up to root
+		do {
+			parts.add(file.getName());
+			file = file.getParentFile();
+		} while (file != null && !file.equals(root));
+
+		for (int i = parts.size() - 1; i >= 0; i--) {
+			prefs = prefs.node((String) parts.get(i));
+		}
+		// now replace it with the result so getPreferences is only called once:
+		obj.defineProperty("preferences", prefs, ScriptableObject.READONLY
+			| ScriptableObject.DONTENUM);
+		return prefs;
+	}
+
 	/**
 	 * Print the string segmentValues of its arguments.
-	 *
-	 * This method is defined as a JavaScript function.
-	 * Note that its arguments are of the "varargs" form, which
-	 * allows it to handle an arbitrary number of arguments
-	 * supplied to the JavaScript function.
-	 *
+	 * 
+	 * This method is defined as a JavaScript function. Note that its arguments
+	 * are of the "varargs" form, which allows it to handle an arbitrary number
+	 * of arguments supplied to the JavaScript function.
+	 * 
 	 */
 	public static void print(Context cx, Scriptable thisObj, Object[] args,
-		Function funObj) {
+			Function funObj) {
 		for (int i = 0; i < args.length; i++) {
 			if (i > 0)
 				System.out.print(", ");
@@ -241,32 +303,31 @@ public class GlobalObject extends ImporterTopLevel {
 	 * Loads and executes a set of JavaScript source files in the current scope.
 	 */
 	public static void include(Context cx, Scriptable thisObj, Object[] args,
-		Function funObj) throws Exception {
-		ScriptographerEngine engine = ScriptographerEngine.getInstance();
+			Function funObj) throws Exception {
 		File baseDir = getDirectory(thisObj);
 		for (int i = 0; i < args.length; i++) {
-			engine.executeFile(new File(baseDir, Context.toString(args[i])), thisObj);
+			ScriptographerEngine.executeFile(new File(baseDir, Context
+				.toString(args[i])), thisObj);
 		}
 	}
 
 	/**
-	 * Loads and executes a set of JavaScript source files in a newly created scope.
+	 * Loads and executes a set of JavaScript source files in a newly created
+	 * scope.
 	 */
 	public static void execute(Context cx, Scriptable thisObj, Object[] args,
-		Function funObj) throws Exception {
-		ScriptographerEngine engine = ScriptographerEngine.getInstance();
+			Function funObj) throws Exception {
 		File baseDir = getDirectory(thisObj);
 		for (int i = 0; i < args.length; i++) {
-			engine.executeFile(new File(baseDir, Context.toString(args[i])), null);
+			ScriptographerEngine.executeFile(new File(baseDir, Context
+				.toString(args[i])), null);
 		}
 	}
 
 	/**
-	 * Evaluates the given javascript string in the current scope.
-	 * Similar to eval(), but it allows the use of another object than
-	 * the global scope:
-	 * e.g.:
-	 * <code>
+	 * Evaluates the given javascript string in the current scope. Similar to
+	 * eval(), but it allows the use of another object than the global scope:
+	 * e.g.: <code>
 	 * var obj = {
 	 *     eval: evaluate
 	 * };
@@ -274,32 +335,39 @@ public class GlobalObject extends ImporterTopLevel {
 	 * </code>
 	 */
 	public static void evaluate(Context cx, Scriptable thisObj, Object[] args,
-		Function funObj) throws Exception {
-		ScriptographerEngine.getInstance().executeString(Context.toString(args[0]), thisObj);
+			Function funObj) throws Exception {
+		ScriptographerEngine.executeString(Context.toString(args[0]), thisObj);
 	}
 
 	/**
-	 *
+	 * 
 	 */
 	public static void commit(Context cx, Scriptable thisObj, Object[] args,
-		Function funObj) {
+			Function funObj) {
 		// call with key set to null so the commit version is not increased
 		// (see CommitManager.commit()
 		CommitManager.commit(null);
 	}
-	
-	public static long getNanoTime(Context cx, Scriptable thisObj, Object[] args,
-		Function funObj) {
+
+	public static long getNanoTime(Context cx, Scriptable thisObj,
+			Object[] args, Function funObj) {
 		return ScriptographerEngine.getNanoTime();
 	}
-	
-	public static Point getMousePoint(Context cx, Scriptable thisObj, Object[] args,
-		Function funObj) {
+
+	public static Point getMousePoint(Context cx, Scriptable thisObj,
+			Object[] args, Function funObj) {
 		return ScriptographerEngine.getMousePoint();
 	}
-	
-	public static boolean updateProgress(Context cx, Scriptable thisObj, Object[] args,
-		Function funObj) {
-		return ScriptographerEngine.updateProgress((long) Context.toNumber(args[0]), (long) Context.toNumber(args[1]));
+
+	public static boolean updateProgress(Context cx, Scriptable thisObj,
+			Object[] args, Function funObj) {
+		return ScriptographerEngine.updateProgress(
+			(long) Context.toNumber(args[0]),
+			(long) Context.toNumber(args[1])
+		);
+	}
+
+	public Scriptable getScope() {
+		return this;
 	}
 }
