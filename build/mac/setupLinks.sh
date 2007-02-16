d24f0e0bed68ebac62fd17feaf6fd5348f0d7ece
Jürg Lehni <juerg@scratchdisk.com>
2007-02-16 01:07:37 +0000

    Added a script to setup symbolic links to java and docs folders for all the different compile targets.

---
+++ build/mac/setupLinks.sh
@@ -0,0 +1,50 @@
+JAVA=../../../../../java/build/
+DOCS=../../../../../../docs/js/
+
+mkdir build/CS3/JVM
+mkdir build/CS3/JVM/Debug
+ln -s $JAVA build/CS3/JVM/Debug/java
+ln -s $DOCS build/CS3/JVM/Debug/docs
+mkdir build/CS3/JVM/Release
+ln -s $JAVA build/CS3/JVM/Release/java
+ln -s $DOCS build/CS3/JVM/Release/docs
+
+mkdir build/CS3/GCJ
+mkdir build/CS3/GCJ/Debug
+ln -s $JAVA build/CS3/GCJ/Debug/java
+ln -s $DOCS build/CS3/GCJ/Debug/docs
+mkdir build/CS3/GCJ/Release
+ln -s $JAVA build/CS3/GCJ/Release/java
+ln -s $DOCS build/CS3/GCJ/Release/docs
+
+mkdir build/CS2/JVM
+mkdir build/CS2/JVM/Debug
+ln -s $JAVA build/CS2/JVM/Debug/java
+ln -s $DOCS build/CS2/JVM/Debug/docs
+mkdir build/CS2/JVM/Release
+ln -s $JAVA build/CS2/JVM/Release/java
+ln -s $DOCS build/CS2/JVM/Release/docs
+
+mkdir build/CS2/GCJ
+mkdir build/CS2/GCJ/Debug
+ln -s $JAVA build/CS2/GCJ/Debug/java
+ln -s $DOCS build/CS2/GCJ/Debug/docs
+mkdir build/CS2/GCJ/Release
+ln -s $JAVA build/CS2/GCJ/Release/java
+ln -s $DOCS build/CS2/GCJ/Release/docs
+
+mkdir build/CS/JVM
+mkdir build/CS/JVM/Debug
+ln -s $JAVA build/CS/JVM/Debug/java
+ln -s $DOCS build/CS/JVM/Debug/docs
+mkdir build/CS/JVM/Release
+ln -s $JAVA build/CS/JVM/Release/java
+ln -s $DOCS build/CS/JVM/Release/docs
+
+mkdir build/CS/GCJ
+mkdir build/CS/GCJ/Debug
+ln -s $JAVA build/CS/GCJ/Debug/java
+ln -s $DOCS build/CS/GCJ/Debug/docs
+mkdir build/CS/GCJ/Release
+ln -s $JAVA build/CS/GCJ/Release/java
+ln -s $DOCS build/CS/GCJ/Release/docs
