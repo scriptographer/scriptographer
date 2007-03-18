b6ac77d2c4d2e1ae31593277c8c221cd4a894405
Jürg Lehni <juerg@scratchdisk.com>
2007-03-18 10:54:00 +0000

    Removed GCJ related folder structure.

--- build/mac/zipPackages.sh
+++ build/mac/zipPackages.sh
@@ -5,7 +5,7 @@ mkdir root
 mv Scriptographer root
 
 # CS
-ditto "$1/CS/JVM/Release/Scriptographer" root/Scriptographer/Scriptographer
+ditto "$1/CS/Release/Scriptographer" root/Scriptographer/Scriptographer
 # For creating zip files: ditto -c -k root Scriptographer_Mac_CS_$2.zip
 
 # Create a DMG file containing the folder
@@ -22,7 +22,7 @@ hdiutil create -fs HFS+ -srcfolder root -volname Scriptographer $DMG
 rm -r root/Scriptographer/Scriptographer
 
 # CS2
-ditto "$1/CS2/JVM/Release/Scriptographer.aip" root/Scriptographer/Scriptographer.aip
+ditto "$1/CS2/Release/Scriptographer.aip" root/Scriptographer/Scriptographer.aip
 #ditto -c -k root Scriptographer_Mac_CS2_$2.zip
 
 # Create a DMG file containing the folder
@@ -40,7 +40,7 @@ rm -r root/Scriptographer/Scriptographer.aip
 
 
 # CS3
-ditto "$1/CS3/JVM/Release/Scriptographer.aip" root/Scriptographer/Scriptographer.aip
+ditto "$1/CS3/Release/Scriptographer.aip" root/Scriptographer/Scriptographer.aip
 
 # Create a DMG file containing the folder
 DMG=Scriptographer_Mac_CS3_$2.dmg
