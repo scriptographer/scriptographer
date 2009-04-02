2aad0ea0ad5a600aae1e7db6d4d87f7d283e45d9
Jürg Lehni <juerg@scratchdisk.com>
2009-04-02 22:35:11 +0000

    Added Process, a prototype that simplifies execution of external processes, and interaction with them, with support for callback functions that are executed when the process provides data, using a Timer so that Illustrator's single-threadedness is not a problem.

---
+++ scripts/libraries/lang/Process.js
@@ -0,0 +1,126 @@
+Process = Base.extend(new function() {
+	function readAll(reader) {
+		if (!reader || !reader.ready())
+			return null;
+		var res = [], line;
+		while (reader.ready() && (line = reader.readLine()) != null)
+			res.push(line);
+		return res.join(java.lang.System.getProperty('line.separator'));
+	}
+
+	var fields = {
+		initialize: function() {
+			this._builder = new java.lang.ProcessBuilder(Array.create(arguments));
+			this.environment = this._builder.environment();
+			this._onData = null;
+			this._onError = null;
+			this._timer = null;
+		},
+
+		start: function() {
+			if (this.directory)
+				this._builder.directory(this.directory);
+			this.process = this._builder.start();
+			this.output = new java.io.PrintStream(this.process.getOutputStream());
+			this.input = new java.io.BufferedReader(new java.io.InputStreamReader(this.process.getInputStream()));
+			this.error = new java.io.BufferedReader(new java.io.InputStreamReader(this.process.getErrorStream()));
+			this.updateTimer();
+		},
+
+		write: function(str) {
+			this.output.print(str);
+			this.output.flush();
+		},
+
+		writeln: function(str) {
+			this.output.println(str);
+			this.output.flush();
+		},
+
+		readln: function() {
+			return reader.ready() && this.input.readLine();
+		},
+
+		readAll: function() {
+			return readAll(this.input);
+		},
+
+		readError: function() {
+			return readAll(this.error);
+		},
+
+		isRunning: function() {
+			try {
+				this.process.exitValue();
+			} catch (e) {
+				return true;
+			}
+			return false;
+		},
+
+		wait: function(timeout) {
+			if (timeout == undefined) {
+				return this.process.waitFor();
+			} else {
+				// Wait for the process to finish.
+				// Unfortunatelly process.waitFor does not know waitFor(timeout)
+				var time = new Date().getTime();
+				while(this.isRunning() && new Date().getTime() - time < timeout)
+					sleep(Math.min(timeout, 10));
+				return this.process.exitValue();
+			}
+		},
+
+		destroy: function() {
+			this.process.destroy();
+		},
+
+		exitValue: {
+			_get: function() {
+				return this.process.exitValue();
+			}
+		},
+
+		updateTimer: function() {
+			if (this.process) {
+				var hasCallback = this._onData || this._onError || this._onDone;
+				if (!this._timer != !hasCallback) { // xor
+					if (hasCallback) {
+						this._timer = (function() {
+							if (this._onError) {
+								var error = this.readError();
+								if (error)
+									this._onError(error);
+							}
+							if (this._onData) {
+								var data = this.readAll();
+								if (data)
+									this._onData(data);
+							}
+							if (!this.isRunning()) {
+								if (this._onDone)
+									this._onDone();
+								this._timer = this._timer.clear();
+							}
+						}).periodic(1, this);
+					} else {
+						this._timer = this._timer.clear();
+					}
+				} 
+			}
+		}
+	};
+	['onError', 'onData', 'onDone'].each(function(name) {
+		var hidden = '_' + name;
+		fields[name] = {
+			_set: function(handler) {
+				this[hidden] = handler;
+				this.updateTimer();
+			},
+			_get: function() {
+				return this[hidden];
+			}
+		};
+	});
+	return fields;
+});
