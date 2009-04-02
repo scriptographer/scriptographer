ba6530a4f53fbd8ea315ca45f469e0187e96a473
Jürg Lehni <juerg@scratchdisk.com>
2009-04-02 22:33:22 +0000

    Added Timer related functionality: - Emulation of Browser style setInterval / setTimeout along with clearInteral / clearTimeout. - Support of Bootstrap's Function#delay / Function#periodic. - A global sleep method.

---
+++ scripts/libraries/lang/timer.js
@@ -0,0 +1,57 @@
+new function() {
+	var timers = {}, current = 1;
+	function createTimer(func, delay, periodic) {
+		var id = current++;
+		var timer = timers[id] = new Timer(delay || 0, periodic);
+		timer.onExecute = typeof func == 'string' ? new Function(func) : func;
+		timer.start();
+		return id;
+	}
+
+	global.setTimeout = function(func, delay) {
+		return createTimer(func, delay, false);
+	}
+
+	global.setInterval = function(func, delay) {
+		return createTimer(func, delay, true);
+	}
+
+	global.clearTimeout = global.clearInterval = function(id) {
+		var timer = timers[id];
+		if (timer) {
+			timer.dispose();
+			delete timers[id];
+		}
+	}
+
+	global.sleep = function(delay) {
+		java.lang.Thread.sleep(delay);
+	}
+}
+
+Function.inject(new function() {
+	function timer(that, periodic, delay, bind, args) {
+		if (delay === undefined)
+			return that.apply(bind, args ? args : []);
+		var fn = that.bind(bind, args);
+		var timer = new Timer(delay, periodic);
+		fn.clear = function() {
+			timer.dispose();
+		};
+		timer.onExecute = fn;
+		timer.start();
+		return fn;
+	}
+
+	return {
+		_generics: true,
+
+		delay: function(delay, bind, args) {
+			return timer(this, false, delay, bind, args);
+		},
+
+		periodic: function(delay, bind, args) {
+			return timer(this, true, delay, bind, args);
+		}
+	}
+});
