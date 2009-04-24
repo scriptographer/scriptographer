8d978c7f038f8329b0e74f25fa944c618628864e
Jürg Lehni <juerg@scratchdisk.com>
2009-04-24 16:24:23 +0000

    Updated Windows side of project files and resources.

--- build/win/zipPackages.bat
+++ build/win/zipPackages.bat
@@ -14,3 +14,8 @@ REM CS3
 copy "%~1\CS3 Release\Scriptographer.aip" Scriptographer
 zip -r Scriptographer_Win_CS3_%2.zip Scriptographer
 del Scriptographer\Scriptographer.aip
+
+REM CS4
+copy "%~1\CS4 Release\Scriptographer.aip" Scriptographer
+zip -r Scriptographer_Win_CS4_%2.zip Scriptographer
+del Scriptographer\Scriptographer.aip
