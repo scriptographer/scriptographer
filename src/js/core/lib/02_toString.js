883390f4553837af05618176ccb18abe141df9e2
Jürg Lehni <juerg@scratchdisk.com>
2009-07-19 23:16:35 +0000

    Added toString for Artboard.

--- src/js/core/lib/02_toString.js
+++ src/js/core/lib/02_toString.js
@@ -1,4 +1,5 @@
-[FillStyle, StrokeStyle, PathStyle].each(function(type) {
+// A list of types that simply redirect toString to toSource(true)
+[FillStyle, StrokeStyle, PathStyle, Artboard].each(function(type) {
 	type.inject({
 		toString: function() {
 			return this.toSource(true);
