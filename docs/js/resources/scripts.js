ca0ae3a517ef1d9aef5f409e3d5bcfea168be22c
Jürg Lehni <juerg@scratchdisk.com>
2009-07-20 21:12:59 +0000

    Doclet: ported over js code from online version.

--- docs/js/resources/scripts.js
+++ docs/js/resources/scripts.js
@@ -1,71 +1,61 @@
-function getVisible(e, def) {
-	if (typeof e == "string") e = document.getElementById(e);
-	if (e) {
-		var d = e.style.display;
-		if (d == 'none') return false;
-		else if (d == '') return def;
-		else return true;
-	}
-	return false;
-}
-
-function setVisible(e, visible) {
-	if (typeof e == "string") e = document.getElementById(e);
-	if (e) e.style.display = visible ? 'block' : 'none';
-}
-
-var lastMemberId = null;
-function toggleMember(id, scrollTo) {
-	if (lastMemberId && lastMemberId != id) {
-		var prevId = lastMemberId;
-		lastMemberId = null;
-		toggleMember(prevId, false);
-	}
-	var link = document.getElementById(id + "-link");
-	if (link != null) {
-		var desc = document.getElementById(id + "-description");
-		var v = getVisible(link, true);
-		lastMemberId = v ? id : null;
-		setVisible(link, !v);
-		setVisible(desc, v);
-		if (scrollTo)
-			setTimeout("scrollToMember('" + id + "');", 1);
-		return false;
-	}
-	return true;
-}
-
-function scrollToMember(id) {
-	var e = document.getElementById(id + "-description");
-	if (e && e.scrollIntoView) {
-		e.scrollIntoView();
-		window.scrollBy(0, -8);
-	} else {
-		document.location.hash = '#' + id;
-	}
-}
-
-function togglePackage(id, def) {
-	var e = document.getElementById("package-" + id);
-	if (e) {
-		var v = getVisible(e, def);
-		setVisible(e, !v);
-		var img = document.images["arrow-" + id];
-		if (img) img.src = "../resources/arrow-" + (v ? "close" : "open") + ".gif";
-	}
-	return false;
-}
-
-window.onload = function()  {
-	var h = document.location.hash;
-	if (h) toggleMember(unescape(h.substring(1)), true);
-}
+// This is  identical with server sided code
 
 $document.addEvent('domready', function() {
- 			$$('code').light({
+	var h = unescape(document.location.hash);
+	if (h) scrollToElement(h.substring(1));
+	$$('pre.js').light({
 		altLines: 'hover',
 		indent: 4,
 		mode: 'ol',
 		fuel: 'js'
 	});
 });
+
+var lastMemberId = null;
+function toggleMember(id, scrollTo) {
+	if (lastMemberId && lastMemberId != id) {
+		var prevId = lastMemberId;
+		lastMemberId = null;
+		toggleMember(prevId);
+	}
+	var link = $('#' + id + '-link');
+	if (link) {
+		var desc = $('#' + id + '-description');
+		var v = !link.hasClass('hidden');
+		lastMemberId = v && id;
+		link.modifyClass('hidden', v);
+		desc.modifyClass('hidden', !v);
+		if (scrollTo)
+			scrollToMember(id);
+		return false;
+	}
+	return true;
+}
+
+function scrollToElement(id) {
+	var e = $('#' + id);
+	if (e) {
+		var offs = e.getOffset();
+		$window.setScrollOffset(offs);
+		if (e.hasClass('member'))
+			toggleMember(id);
+	} else {
+		document.location.hash = id;
+	}
+}
+
+function togglePackage(id, def) {
+	var e = $('#package-' + id);
+	if (e) {
+		var v = !e.hasClass('hidden');
+		e.modifyClass('hidden', v);
+		var img = document.images['arrow-' + id];
+		if (img) img.src = img.src.replace(/open|close/, v ? 'close' : 'open');
+	}
+	return false;
+}
+
+function toggleThumbnail(id, over) {
+	$('#' + id).modifyClass('hidden', over);
+	$('#' + id + '_over').modifyClass('hidden', !over);
+}
