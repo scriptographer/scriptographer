7d58c41c2e0f8b6384c37b2da4c4b2a8cfa61577
Jürg Lehni <juerg@scratchdisk.com>
2010-03-23 18:50:10 +0000

    getStories sometimes seem to return null. This prevents an exception but does not solve the issue.

--- src/java/com/scriptographer/ai/TextItem.java
+++ src/java/com/scriptographer/ai/TextItem.java
@@ -191,7 +191,10 @@ public abstract class TextItem extends Item {
 		// to the same Story, so things can be cached there:
 		int index = getStoryIndex();
 		ReadOnlyList list = document.getStories();
-		if (index >= 0 && index < list.size())
+		// getStories sometimes seem to return null. This prevents an
+		// exception but does not solve the issue.
+		// TODO: Find out why list can be null here.
+		if (list != null && index >= 0 && index < list.size())
 			return (TextStory) list.get(index);
 		return null;
 	}
