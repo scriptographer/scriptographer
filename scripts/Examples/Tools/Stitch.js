4c4e24d9c2df5a5bed2dc1fca84a0174eb2a048e
Jürg Lehni <juerg@scratchdisk.com>
2010-05-12 19:16:53 +0100

    Updated Stitch.js to work with new Tool#fixedDistance feature.

--- scripts/Examples/Tools/Stitch.js
+++ scripts/Examples/Tools/Stitch.js
@@ -1,46 +1,43 @@
-//////////////////////////////////////////////////////////////////////////////
+tool.fixedDistance = 3;
+
+////////////////////////////////////////////////////////////////////////////////
 // Interface:
 
 var values = {
-	distance: 3,
+	distance: tool.fixedDistance,
 	size: 10
 };
 
 var components = {
-	distance: { label: 'Distance' },
-	size: { label: 'Size' }
+	distance: {
+		label: 'Distance', type: 'number',
+		units: 'point', range: [1, 100],
+		onChange: function(value) {
+			tool.fixedDistance = value;
+		}
+	},
+	size: {
+		label: 'Size', type: 'number',
+		units: 'point', range: [1, 100]
+	}
 };
 
 var palette = new Palette('Stitch', components, values);
 
-//////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 // Mouse handling:
 
-var mul, path, res;
+var path;
+var mul = 1;
+
 function onMouseDown(event) {
-	mul = 1;
 	path = new Path();
-	path.add(event.point);
-	res = new Path();
 }
 
 function onMouseDrag(event) {
-	path.add(event.point);
-	if (path.length > 10) {
-		path.pointsToCurves();
-		path.curvesToPoints(values.distance, 10000);
-		for (var i = 0, j = path.curves.length; i < j; i++) {
-			var bezier = path.curves[i];
-			var pt = bezier.getPoint(0);
-			var n = bezier.getNormal(1);
-			if (n.x != 0 || n.y != 0) {
-				n = n.normalize(values.size);
-				res.add(pt + (n * mul));
-				mul *= -1;
-			}
-		}
-		path.remove();
-		path = new Path();
-		path.add(event.point);
-	}
+	var step = event.delta;
+	step.angle += mul * (90).toRadians();
+	step.length = values.size;
+	path.add(event.point + step);
+	mul *= -1;
 }
