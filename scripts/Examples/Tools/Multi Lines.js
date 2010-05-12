65f98f2b51cae4a41e96b9d2ca0728aac1da1747
Jonathan Puckey <me@jonathanpuckey.com>
2010-05-12 19:41:21 +0100

    Added Multi Lines to Tools

---
+++ scripts/Examples/Tools/Multi Lines.js
@@ -0,0 +1,67 @@
+//////////////////////////////////////////////////////////////////////////////
+// Values:
+
+tool.minDistance = 10;
+
+var values = {
+	lines: 6,
+	size: 20,
+	happy: false,
+	smooth: true
+};
+
+//////////////////////////////////////////////////////////////////////////////
+// Interface:
+
+var components = {
+	lines: {
+		label: 'Lines',
+		range: [0, 100]
+	},
+	size: {
+		label: 'Size',
+		min: 0
+	},
+	smooth: { label: 'Smooth' },
+	happy: { label: 'Happy' }
+};
+
+var palette = new Palette('Multi Lines', components, values);
+
+//////////////////////////////////////////////////////////////////////////////
+// Mouse handling:
+
+var paths;
+function onMouseDown(event) {
+	paths = [];
+	for (var i = 0; i < values.lines; i++) {
+		var path = new Path();
+		path.fillColor = null;
+		path.closed = true;
+		if (values.happy) {
+			var swatch = document.swatches[(i + 4) % document.swatches.length];
+			path.fillColor = swatch.color;
+			path.strokeColor = null;
+		} else {
+			path.strokeColor = document.currentStyle.strokeColor || '#000000';
+		}
+		paths.push(path);
+	}
+}
+
+function onMouseDrag(event) {
+	
+	var offset = event.delta;
+	offset.angle += (90).toRadians();
+
+	var lineSize = values.size / values.lines;
+	for (var i = 0; i < values.lines; i++) {
+		var path = paths[values.lines - 1 - i];
+		offset.length = (lineSize * i + lineSize / 2);
+		path.add(event.middlePoint + offset);
+		path.insert(0, event.middlePoint - offset);
+
+		if (values.smooth)
+			path.smooth();
+	}
+}
