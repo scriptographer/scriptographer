91b423d916febe31e462a6438308b46a3fa8d310
Jürg Lehni <juerg@scratchdisk.com>
2010-05-24 17:09:09 +0100

    Add Point/Size.min/max and remove dysfunctional Rectangle extensions.

--- src/js/core/lib/ai/Math.js
+++ src/js/core/lib/ai/Math.js
@@ -4,11 +4,16 @@ Point.inject(new function() {
 			return new Point(Math[name](this.x), Math[name](this.y));
 		};
 	}, {
-		statics: {
+		statics: ['min', 'max'].each(function(name) {
+			this[name] = function(pt1, pt2) {
+				return new Point(Math[name](pt1.x, pt2.x),
+						Math[name](pt1.y, pt2.y));
+			};
+		}, {
 			random: function() {
 				return new Point(Math.random(), Math.random());
 			}
-		}
+		})
 	});
 });
 
@@ -18,18 +23,15 @@ Size.inject(new function() {
 			return new Size(Math[name](this.width), Math[name](this.height));
 		};
 	}, {
-		statics: {
+		statics: ['min', 'max'].each(function(name) {
+			this[name] = function(size1, size2) {
+				return new Size(Math[name](size1.width, size2.width),
+						Math[name](size1.height, size2.height));
+			};
+		}, {
 			random: function() {
 				return new Size(Math.random(), Math.random());
 			}
-		}
+		})
 	});
-});
-
-Rectangle.inject(new function() {
-	return ['round', 'ceil', 'floor', 'abs'].each(function(name) {
-		this[name] = function() {
-			return new Rectangle(Math[name](this.point), Math[name](this.size));
-		};
-	}, {});
 });
