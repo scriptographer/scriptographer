0bc697652750278ac69ca2ed62efa24a04dbfd5e
Jürg Lehni <juerg@scratchdisk.com>
2010-05-29 19:20:46 +0100

    Add Timer class file forgotten in previous commit.

---
+++ src/java/com/scriptographer/ui/Timer.java
@@ -0,0 +1,147 @@
+/*
+ * Scriptographer
+ *
+ * This file is part of Scriptographer, a Plugin for Adobe Illustrator.
+ *
+ * Copyright (c) 2002-2010 Juerg Lehni, http://www.scratchdisk.com.
+ * All rights reserved.
+ *
+ * Please visit http://scriptographer.org/ for updates and contact.
+ *
+ * -- GPL LICENSE NOTICE --
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * -- GPL LICENSE NOTICE --
+ * 
+ * File created on May 29, 2010.
+ *
+ * $Id$
+ */
+
+package com.scriptographer.ui;
+
+import com.scratchdisk.script.Callable;
+import com.scratchdisk.util.IntMap;
+import com.scriptographer.ScriptographerEngine;
+import com.scriptographer.ScriptographerException;
+import com.scriptographer.sg.Script;
+
+/**
+ * @author lehni
+ *
+ */
+public class Timer extends NativeObject {
+
+	private Dialog dialog;
+	private int period;
+	private boolean periodic;
+
+	private Script script = null;
+
+	private static IntMap<Timer> timers = new IntMap<Timer>();
+
+	/**
+	 * Creates a timer object.
+	 * 
+	 * @param period The timer's period in milliseconds.
+	 * @param periodic Controls whether the timer is one-shop or periodic. 
+	 */
+	public Timer(Dialog dialog, int period, boolean periodic) {
+		script = ScriptographerEngine.getCurrentScript();
+		this.dialog = dialog;
+		this.period = period;
+		this.periodic = periodic;
+		handle = dialog.nativeCreateTimer(period);
+		if (handle == 0)
+			throw new ScriptographerException("Unable to create Timer.");
+		timers.put(handle, this);
+	}
+
+	public Timer(Dialog dialog, int period) {
+		this(dialog, period, true);
+	}
+
+	public void abort() {
+		if (handle != 0) {
+			timers.remove(handle);
+			dialog.nativeAbortTimer(handle);
+			handle = 0;
+		}
+	}
+	
+	public double getPeriod() {
+		return period;
+	}
+	
+	public boolean isPeriodic() {
+		return periodic;
+	}
+
+	protected boolean canAbort(boolean ignoreKeepAlive) {
+		return script == null || script.canRemove(ignoreKeepAlive);
+	}
+
+	public static void abortAll(boolean ignoreKeepAlive, boolean force) {
+		// As abort() modifies the Map, using an iterator is not possible here:
+		Object[] timers = Timer.timers.values().toArray();
+		for (int i = 0; i < timers.length; i++) {
+			Timer timer = (Timer) timers[i];
+			if (force || timer.canAbort(ignoreKeepAlive))
+				timer.abort();
+		}
+	}
+	
+	private Callable onExecute = null;
+
+	public void setOnExecute(Callable onExecute) {
+		this.onExecute = onExecute;
+	}
+	
+	public Callable getOnExecute() {
+		return onExecute;
+	}
+
+	protected void onExecute() {
+		if (onExecute != null)
+			ScriptographerEngine.invoke(onExecute, this);
+	}
+
+	/**
+	 * To be called from the native environment:
+	 */
+	@SuppressWarnings("unused")
+	private static void onExecute(int handle) {
+		Timer timer = getTimer(handle);
+		if (timer != null) {
+			// Simulate one shot timers:
+			if (!timer.periodic)
+				timer.abort();
+			timer.onExecute();
+		}
+	}
+
+	private static Timer getTimer(int handle) {
+		return (Timer) timers.get(handle);
+	}
+	
+	protected void finalize() {
+		abort();
+	}
+
+	public Object getId() {
+		// Return the integer handle as id instead of a formated string, as the
+		// script side wants to work with numeric timer ids.
+		return handle;
+	}
+}
