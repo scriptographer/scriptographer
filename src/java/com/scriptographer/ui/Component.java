a3d1fe36c9b6c7d0dd22edc8b72ed31140f03d0d
Jürg Lehni <juerg@scratchdisk.com>
2010-05-31 01:12:14 +0100

    Drawer#getTextWidth / Height is not line break aware on Windows, so go back to previous version of Component#getTextSize() that splits into lines.

--- src/java/com/scriptographer/ui/Component.java
+++ src/java/com/scriptographer/ui/Component.java
@@ -82,15 +82,29 @@ abstract class Component extends NotificationHandler {
 		Image image = new Image(1, 1, ImageType.RGB);
 		Drawer drawer = image.getDrawer();
 		drawer.setFont(getFont());
-		if (ignoreBreaks)
-			text = text.replaceAll("\r\n|\n|\r", " ");
-		// Calculate the size of this part, using the drawer
-		int width = drawer.getTextWidth(text);
-		if (maxWidth > 0 && width > maxWidth)
-			width = maxWidth;
-		int height = drawer.getTextHeight(text, width + 1);
+		// Split at new lines chars, and measure each line separately
+		String[] lines = ignoreBreaks ? new String[] {
+			text.replaceAll("\r\n|\n|\r", " ")
+		} : text.split("\r\n|\n|\r");
+		Size size = new Size(0, 0);
+		for (int i = 0; i < lines.length; i++) {
+			String line = lines[i];
+			if (line.length() == 0)
+				line = " "; // Make sure empty lines are measured too
+
+			// Calculate the size of this part, using the drawer
+			int width = drawer.getTextWidth(line);
+			if (maxWidth > 0 && width > maxWidth)
+				width = maxWidth;
+			int height = drawer.getTextHeight(line, width + 1);
+
+			// And add up size
+			if (width > size.width)
+				size.width = width;
+			size.height += height;
+		}
 		drawer.dispose();
-		return new Size(width, height);
+		return size;
 	}
 
 	public Size getTextSize(String text) {
