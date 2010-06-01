500be7ff54d7dfa03ec6c5b00d5fb5fa107400f6
Jürg Lehni <juerg@scratchdisk.com>
2010-06-01 17:03:57 +0100

    index on master: 23a971c Clean up native Point/Rectangle/Size conversion to avoid ADM and AI type mix-ups.

---
+++ src/java/com/scriptographer/ui/Dialog.java
@@ -0,0 +1,137 @@
+/*
+ * Scriptographer
+ *
+ * This file is part of Scriptographer, a Plugin for Adobe Illustrator.
+ *
+ * Copyright (c) 2002-2010 Juerg Lehni, http://www.scratchdisk.com.
+ * All rights reserved.
+ *
+ * Please visit http://scriptographer.org/ for updates and contact.
+ *
+ * -- GPL LICENSE NOTICE --
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * -- GPL LICENSE NOTICE --
+ * 
+ * File created on Jun 1, 2010.
+ */
+
+package com.scriptographer.ui;
+
+import java.io.File;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.scriptographer.adm.PaletteComponent;
+import com.scriptographer.ai.Color;
+
+/**
+ * @author lehni
+ *
+ */
+public class Dialog {
+	public static void alert(String title, String message) {
+		UIFactory.getInstance().alert(title, message);
+	}
+
+	public static void alert(String message) {
+		alert("Scriptographer", message);
+	}
+
+	public static boolean confirm(String title, String message) {
+		return UIFactory.getInstance().confirm(title, message);
+	}
+
+	public static boolean confirm(String message) {
+		return confirm("Scriptographer", message);
+	}
+
+	public static Map<String, Object> prompt(String title,
+			Map<String, Map> components, Map<String, Object> values) {
+		Component[] comps =
+				Component.getComponents(components, values);
+		Object[] results = prompt(title, comps);
+		if (results != null) {
+			if (values == null)
+				values = new LinkedHashMap<String, Object>();
+			for (int i = 0; i < comps.length; i++)
+				values.put(comps[i].getName(), results[i]);
+		}
+		return values;
+	}
+
+	public static Map<String, Object> prompt(String title,
+			Map<String, Map> items) {
+		return prompt(title, items, null);
+	}
+
+	/**
+	 * @jshide
+	 */
+	public static Object[] prompt(String title, Component[] components) {
+		return UIFactory.getInstance().prompt(title, components);
+	}
+
+	public static File fileOpen(String message, String[] filters,
+			File selectedFile) {
+		// TODO: Implement through a factory
+		return null;
+	}
+
+	public static File fileOpen(String message, String[] filters) {
+		return fileOpen(message, filters, null);
+	}
+
+	public static File fileOpen(String message) {
+		return fileOpen(message, null, null);
+	}
+
+	public static File fileOpen() {
+		return fileOpen(null, null, null);
+	}
+
+	public static File fileSave(String message, String[] filters,
+			File selectedFile) {
+		// TODO: Implement through a factory
+		return null;
+	}
+
+	public static File fileSave(String message, String[] filters) {
+		return fileSave(message, filters, null);
+	}
+
+	public static File fileSave(String message) {
+		return fileSave(message, null, null);
+	}
+
+	public static File fileSave() {
+		return fileSave(null, null, null);
+	}
+
+	public static native File chooseDirectory(String message, File selectedDir);
+
+	public static File chooseDirectory(String message) {
+		return chooseDirectory(message, null);
+	}
+
+	public static File chooseDirectory() {
+		return chooseDirectory(null, null);
+	}
+
+	public static native Color chooseColor(Color color);
+
+	public static Color chooseColor() {
+		return chooseColor(null);
+	}
+}
