31921de80e3a88651004c47f52e201d2dc6ccefe
Jürg Lehni <juerg@scratchdisk.com>
2010-06-08 17:04:42 +0100

    Clean up build.xml and change zipPackage.sh to create package directly in versioned subdirectories.

--- build/mac/zipPackage.sh
+++ build/mac/zipPackage.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
-# For creating zip files instead use: ditto -c -k root Scriptographer_Mac_CS_$2.zip
-
 # Create the root of the DMG, move the folder there
 
 PACKAGES=$1
 TARGET=$2
 VERSION=$3
 
-# create root for package
+# Create root for package
 mkdir root
 mv Scriptographer root
+
+# Create destination folder for version if it does not exist
+
+if [ ! -d ${VERSION} ]
+then
+	mkdir ${VERSION}
+fi
+
 
 # CS has a different filename than the others
 if [ $TARGET == "CS" ]
@@ -20,11 +26,11 @@ else
 	PLUGIN=Scriptographer.aip
 fi
 
-# copy with all attributes, even as packages (folders)
+# Copy with all attributes, preserving packaged folders
 ditto "$PACKAGES/$TARGET/Release/$PLUGIN" root/Scriptographer/$PLUGIN
 
 # Create a DMG file containing the folder
-DMG=Scriptographer_Mac_${TARGET}_${VERSION}.dmg
+DMG=${VERSION}/Scriptographer_Mac_${TARGET}_${VERSION}.dmg
 
 # Remove it if it exists already
 if [ -e $DMG ]
@@ -33,6 +39,8 @@ then
 fi
 
 hdiutil create -fs HFS+ -srcfolder root -volname Scriptographer $DMG
+
+# To create zip files instead use: ditto -c -k root $ZIP
 
 rm -r root/Scriptographer/$PLUGIN
 
