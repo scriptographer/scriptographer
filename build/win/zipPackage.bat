423671d37a3bf2920bce8127798365166a55fdf9
Jürg Lehni <juerg@scratchdisk.com>
2010-06-08 17:19:54 +0100

    Change zipPackage.bat to create package directly in versioned subdirectories.

--- build/win/zipPackage.bat
+++ build/win/zipPackage.bat
@@ -1,5 +1,10 @@
 @echo off
 
+if not exist "%3" (
+	mkdir "%3"
+)
+
+
 copy "%~1\%2 Release\Scriptographer.aip" Scriptographer
-zip -r Scriptographer_Win_%2_%3.zip Scriptographer
+zip -r %3\Scriptographer_Win_%2_%3.zip Scriptographer
 del Scriptographer\Scriptographer.aip
