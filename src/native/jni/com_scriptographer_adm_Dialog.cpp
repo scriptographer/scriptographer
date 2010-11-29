57c920dac1ad086e802b633e06bd0787d2f7b421
Jürg Lehni <juerg@scratchdisk.com>
2010-11-29 17:15:39 +0000

    Remove unused nativeGetMinimum/MaximumSize().

--- src/native/jni/com_scriptographer_adm_Dialog.cpp
+++ src/native/jni/com_scriptographer_adm_Dialog.cpp
@@ -307,21 +307,6 @@ JNIEXPORT void JNICALL Java_com_scriptographer_adm_Dialog_setPosition(
 }
 
 /*
- * com.scriptographer.adm.Size nativeGetimumMinSize()
- */
-JNIEXPORT jobject JNICALL Java_com_scriptographer_adm_Dialog_nativeGetMinimumSize(
-		JNIEnv *env, jobject obj) {
-	try {
-		ADMDialogRef dialog = gEngine->getDialogHandle(env, obj);
-		ADMPoint pt;
-		pt.h = sADMDialog->GetMinWidth(dialog);
-		pt.v = sADMDialog->GetMinHeight(dialog);
-		return gEngine->convertSize(env, &pt);
-	} EXCEPTION_CONVERT(env);
-	return NULL;
-}
-
-/*
  * void nativeSetMinimumSize(int width, int height)
  */
 JNIEXPORT void JNICALL Java_com_scriptographer_adm_Dialog_nativeSetMinimumSize(
@@ -331,20 +316,6 @@ JNIEXPORT void JNICALL Java_com_scriptographer_adm_Dialog_nativeSetMinimumSize(
 		sADMDialog->SetMinWidth(dialog, width);
 		sADMDialog->SetMinHeight(dialog, height);
 	} EXCEPTION_CONVERT(env);
-}
-
-/*
- * com.scriptographer.adm.Size nativeGetMaximumSize()
- */
-JNIEXPORT jobject JNICALL Java_com_scriptographer_adm_Dialog_nativeGetMaximumSize(
-		JNIEnv *env, jobject obj) {
-	try {
-		ADMDialogRef dialog = gEngine->getDialogHandle(env, obj);
-		DEFINE_ADM_POINT(pt, sADMDialog->GetMaxWidth(dialog),
-				sADMDialog->GetMaxHeight(dialog));
-		return gEngine->convertSize(env, &pt);
-	} EXCEPTION_CONVERT(env);
-	return NULL;
 }
 
 /*
